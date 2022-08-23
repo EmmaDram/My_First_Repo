166636a5e1c2542fa761ac74c82341910fb55f9e
EmmaDram <esdram@stu.naperville203.org>
2022-08-23 12:38:57 -0500

    started ch1 notes in bluej

---
+++ My First bluej Project/chapter1Notes.java
@@ -0,0 +1,42 @@
+
+/**
+ * Emma Dram
+ * 8-23-22 (Tuesday)
+ * Notes for chapter 1 where we are going to take some code notes
+ * Helpers: Mr. Hayes
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+public class chapter1Notes
+{
+    
+    /**
+     * main method
+     * where we are going to code
+     */
+    public static void Main(String[] args)
+    {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        //these are the basic data types
+        int x = 9, y, z;
+        double num = 5, num1 = 23.4567, num2;
+        char letter = 'a' //cannot put two chars in one variable, must use ''
+        boolean choice = false;
+        float anum = 7; //can't initialize with a decimal
+        long a = 9; //takes up more memory
+        short b = 5; //takes up less memory
+        //longs are used for storing longer integers
+        //shorts are used for storing shorter integers
+        
+        
+        //not a basic basic data type - these are objects!
+        String word = new String("word"); //series of chars
+        Integer number = new Integer(47);
+        Double number2 = new Double(34.5)
+        
+        
+    }
+}
